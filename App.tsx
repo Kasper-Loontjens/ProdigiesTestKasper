68935a9f211b1a68a3cc55144b4aac1a43ab464b
Kasper-Loontjens <112395890+Kasper-Loontjens@users.noreply.github.com>
2024-01-11 11:46:47 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useState } from 'react';
+
+import CoreNavigation from './src/navigations/CoreNavigation';
+import { Text, View } from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+
+  return (
+    <CoreNavigation/>
+  );
+}
+
+export default App;
